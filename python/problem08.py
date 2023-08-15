7ef8d43e666b97da71fddbca56319bfb58a69e6e
aashishred <95857097+aashishred@users.noreply.github.com>
2023-08-15 15:41:27 +0100

    Add files via upload

---
+++ python/problem08.py
@@ -0,0 +1,61 @@
+import time, tracemalloc  # Used for efficiency tracking of elapsed time and memory usage
+import pyperclip  # Used to automatically copy the answer to clipboard
+import os  # Used to print the name of the problem (i.e., file name)
+
+
+# Largest Product in a Series 
+target = 13
+
+
+class Solution1:
+    def __init__(self):
+        self.answer = 0  # Initialise answer variable to 0
+        self.answers = []
+        self.number = """73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450"""
+        for digit in "\n":
+            self.number = self.number.replace(digit, '')
+        self.length = len(self.number)
+
+    def solve(self):
+        for i in range(self.length - target): 
+            self.answer = 1
+            for j in range(target):
+                self.answer *= int(self.number[i+j])
+            self.answers.append(self.answer)
+        return max(self.answers)
+
+
+if __name__ == "__main__":
+    tracemalloc.start()  # Start memory allocation trace
+    start = time.time()  # Start timer
+
+    solution = Solution1()  # Change Solution1 to Solution2 etc., to run a different solution
+    answer = solution.solve()  # Runs the solution
+
+    print("Total elapsed time:", time.time() - start)  # Prints total time taken to run the solve() method
+    print("Memory Usage\tCurrent:", tracemalloc.get_traced_memory()[0], "\tPeak:", tracemalloc.get_traced_memory()[1])  # Prints the current and peak memory usage in bytes of the traced Python objects
+
+    tracemalloc.stop()  # Stops the memory allocation trace
+
+    print("\n", os.path.basename(__file__))  # Prints the name of the problem (file name)
+    print("Answer:", answer)  # Prints the answer returned by the solution
+    pyperclip.copy(answer)  # Copies the answer to clipboard (to be pasted directly into Project Euler).
